7a47d93b26619bd8a382e0ac9ff0a3e209e07319
Sayan47 <sayrit89ss@gmail.com>
2020-06-05 22:41:10 +0530

    first commit

---
+++ Assets/Scripts/DrawerSound.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawerSound : MonoBehaviour
+{
+    public AudioSource do_AudioSource;
+    public AudioSource dc_AudioSource;
+    bool flag = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnEnable()
+    {
+        GetComponent<VRActionHarness>().OnActionTrigger += ToggleAudio;
+    }
+    virtual protected void ToggleAudio()
+    {
+        if (!flag)
+        {
+            do_AudioSource.Play();
+            flag = true;
+        }
+        else
+        {
+            dc_AudioSource.Play();
+            flag = false;
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerGrab.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class PlayerGrab : MonoBehaviour
+{
+    public GameObject tab;
+    public GameObject myHand;
+    public GameObject temp_obj;
+    public Camera myCamera;
+    public bool flag = false;
+
+    void OnEnable()
+    {
+        if (flag == false)
+        {
+            GetComponent<VRActionHarness>().OnActionTrigger += Grab;
+        }
+        
+    }
+
+    void OnDisable()
+    {
+    }
+    void Update()
+    {
+        if (flag == true)
+        {
+            temp_obj.transform.position = myHand.transform.position;
+        }
+
+    }
+    void Grab()
+    {
+        Rigidbody tabRigidBody = tab.GetComponent<Rigidbody>();
+        if (flag == false)
+        {
+            //tab.transform.position =new Vector3(myCamera.transform.position.x + 4.6329f, myCamera.transform.position.y - 2.5884f, myCamera.transform.position.z -2.20524f);
+            tabRigidBody.isKinematic = true;
+            tab.transform.SetParent(myHand.transform);
+            tab.transform.localPosition = myHand.transform.localPosition;
+            temp_obj.transform.position = myHand.transform.position;
+            tab.transform.SetParent(temp_obj.transform);
+            flag = true;
+        }
+        else
+        {
+            tab.transform.position = new Vector3(20f, 13f, -4.1f);
+            tabRigidBody.isKinematic = false;
+            flag = false;
+        }
+        
+    }
+    
+}
